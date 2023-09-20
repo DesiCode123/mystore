68438863d93475474fe13e4ce79d4d8be1279527
DesiCode123 <sharmila@live.no>
2023-09-20 21:29:33 +0200

    Update controller for products

--- src/main/java/com/sharmila/mystore/entities/Order.java
+++ src/main/java/com/sharmila/mystore/entities/Order.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -32,9 +31,6 @@ public class Order {
     @OneToOne(cascade = CascadeType.ALL,orphanRemoval = true)
     @JoinColumn(name = "paymentId")
     private Payment payment;
-
-
-
 
 
 }
